b536103073d45f5fa8d4147d3e001b2c4dcb6b1a
Nafis Siddik Bhamjee <P17157542@GWHXX652LIN019W.lec-admin.dmu.ac.uk>
2018-11-15 14:47:48 +0000

    introduced a third line

--- Hello.java
+++ Hello.java
@@ -3,8 +3,9 @@ package primitives;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Fare Thee well");
 		System.out.println("Hello World");
-
+		System.out.println("Quicker version!");
 	}
 
 }
